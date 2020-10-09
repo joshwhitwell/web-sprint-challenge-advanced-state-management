404009cd7764661a975f6394cee0f199893b28d6
Joshua Whitwell <josh.whitwell@gmail.com>
2020-10-09 13:06:53 -0400

    created JSX to render smurfs

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -20,11 +20,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! W/Redux</h1>
-        <p>{this.props.state.test}</p>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <h1>SMURFS</h1>
+        {this.props.state.smurfs.map(smurf =>
+          <div key={smurf.id}>
+            <h2>{smurf.name}</h2>
+            <p>Age: {smurf.age}</p>
+            <p>Height: {smurf.height}</p>
+          </div>
+          )
+        }
       </div>
     )
   }
